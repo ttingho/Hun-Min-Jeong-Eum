9739d6c156cbb8bce16be5563cbc24b0533d0e8f
gguggu <adrw1365@gmail.com>
2019-07-31 19:18:19 +0900

    [ADD] SignUp
    SignUp 회원가입 페이지 생성만 했어여

---
+++ Hun-Min-Jeong-Eum-Front/src/containers/SignUp/SignUp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+    render() {
+        return (
+            <div>
+                안녕 사인업
+            </div>
+        );
+    }
+}
+
+export default SignUp;
---
+++ Hun-Min-Jeong-Eum-Front/src/pages/SignUpPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignUp from 'containers/SignUp/SignUp';
+
+const SignUpPage = () => {
+    return (
+        <SignUp>
+            
+        </SignUp>
+    );
+};
+
+export default SignUpPage;
