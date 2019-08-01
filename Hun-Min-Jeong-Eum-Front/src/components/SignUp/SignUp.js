44c64e9578f7ac3e0f8a498d6cd3b924a4caf44b
gguggu <adrw1365@gmail.com>
2019-08-01 21:36:37 +0900

    [FIX] SignUp
    회원가입 구현 중이에여

--- Hun-Min-Jeong-Eum-Front/src/components/SignUp/SignUp.js
+++ Hun-Min-Jeong-Eum-Front/src/components/SignUp/SignUp.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
 class SignUp extends Component {
+  constructor (props){
+    super(props);
+    this.state = {};
+  }
+
   render () {
     return (
-      <div>
-        안녕 사인업
+      <div className="SignUp">
+        <p className="SignUp--title">회원가입</p>
+        <div>
+          <button className="SignUp--teacherButton"></button>
+          <button className="SignUp--studentButton"></button>
+          <div>
+            <p>{}</p>
+          </div>
+        </div>
       </div>
     );
   }
