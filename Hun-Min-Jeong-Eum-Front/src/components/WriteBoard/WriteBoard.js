15b007b0a8603c84335cf3b4ed91e3c4dfab797d
ttingho <myeongho020812@gmail.com>
2019-08-02 19:45:27 +0900

    [FIX] WriteBoard.js
    문법 오류들을 고쳐주었습니다.

--- Hun-Min-Jeong-Eum-Front/src/components/WriteBoard/WriteBoard.js
+++ Hun-Min-Jeong-Eum-Front/src/components/WriteBoard/WriteBoard.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
 
 class WriteBoard extends Component {
-    render() {
-        return (
-            <div className="WriteBoard">
-                <div className="WriteBoard-header">
-                    <div className="WriteBoard-header-cancle"></div>
-                    <div className="WriteBoard-header-image"></div>
-                    <div className="WriteBoard-header-tools">
-                        <div className="WriteBoard-header-tools-pen"></div>
-                        <div className="WriteBoard-header-tools-pen"></div>
-                        <div className="WriteBoard-header-tools-pen"></div>
-                        <div className="WriteBoard-header-tools-Color">
-                            <div className="WriteBoard-header-tools-Color-current"></div>
-                            <div className="WriteBoard-header-tools-Color-black"></div>
-                            <div className="WriteBoard-header-tools-Color-red"></div>
-                            <div className="WriteBoard-header-tools-Color-blue"></div>
-                        </div>
-                    </div>
-                    <div className="WriteBoard-header-download"></div>
-                </div>
+  render () {
+    return (
+      <div className="WriteBoard">
+        <div className="WriteBoard-header">
+          <div className="WriteBoard-header-cancle"></div>
+          <div className="WriteBoard-header-image"></div>
+          <div className="WriteBoard-header-tools">
+            <div className="WriteBoard-header-tools-pen"></div>
+            <div className="WriteBoard-header-tools-pen"></div>
+            <div className="WriteBoard-header-tools-pen"></div>
+            <div className="WriteBoard-header-tools-Color">
+              <div className="WriteBoard-header-tools-Color-current"></div>
+              <div className="WriteBoard-header-tools-Color-black"></div>
+              <div className="WriteBoard-header-tools-Color-red"></div>
+              <div className="WriteBoard-header-tools-Color-blue"></div>
             </div>
-        );
-    }
+          </div>
+          <div className="WriteBoard-header-download"></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default WriteBoard;
