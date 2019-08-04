e0c05e6d75c54cd0199b0d8ddd9a72da96ad817d
gguggu <adrw1365@gmail.com>
2019-08-05 00:33:01 +0900

    [FIX] LookBoard
    LookBoard 디자인 오류 수정했어여

--- Hun-Min-Jeong-Eum-Front/src/components/LookBoard/LookBoard.js
+++ Hun-Min-Jeong-Eum-Front/src/components/LookBoard/LookBoard.js
@@ -8,18 +8,18 @@ const LookBoard = ({ goingBack }) => {
         들어갈 내용들ㅇㅂㅇ
       </div>
       <div className="LookBoard--equipBar">
-        <button className="LookBoard--equipBar--pencil"></button>
-        <button className="LookBoard--equipBar--highlighter"></button>
-        <button className="LookBoard--equipBar--eraser"></button>
+        <button className="LookBoard--equipBar--pencil" name="parentButton"></button>
+        <button className="LookBoard--equipBar--highlighter" name="parentButton"></button>
+        <button className="LookBoard--equipBar--eraser" name="parentButton"></button>
         <div className="LookBoard--equipBar--colorButtons">
-          <button className="LookBoard--equipBar--colorButtons--choosedColor" id="choosed"></button>
+          <button className="LookBoard--equipBar--colorButtons--choosedColor" name="choosed"></button>
           <div className="LookBoard--equipBar--colorButtons--choosingButtons">
             <button className="LookBoard--equipBar--colorButtons--choosingButtons--black" name="choosing"></button>
             <button className="LookBoard--equipBar--colorButtons--choosingButtons--red" name="choosing"></button>
             <button className="LookBoard--equipBar--colorButtons--choosingButtons--blue" name="choosing"></button>
           </div>
         </div>
-        <button className="LookBoard--equipBar--goBack" onClick={goingBack}></button>
+        <button className="LookBoard--equipBar--goBack" onClick={goingBack} ></button>
         <button className="LookBoard--equipBar--saveButton"></button>
       </div>
     </div>
