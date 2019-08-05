4b09ffad6ad3712b390f654d6878098889b11b39
ttingho <myeongho020812@gmail.com>
2019-08-05 19:13:30 +0900

    [FIX] MainPage
    선생님 용과 학생용과의 전환 과정에서 색이 이상하게 변하는 오류를 수정했습니다.

--- Hun-Min-Jeong-Eum-Front/src/components/Main/MainContents/SubjectContents/SubjectContents.js
+++ Hun-Min-Jeong-Eum-Front/src/components/Main/MainContents/SubjectContents/SubjectContents.js
@@ -1,31 +1,34 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import styles from './SubjectContents.scss';
 import classNames from 'classnames/bind';
 import SubjectCard from '../../MainCard/SubjectCard';
 
 const cx = classNames.bind(styles);
 
-const SubjectContents = (SubjectData, MainMember) => {
-  const SubjectList = SubjectData.SubjectData;
-  const MainColor = MainMember.MainMember;
-  return (
-    <Fragment>
-      <div className={ cx('SubjectContents') }>
-        <div className={ cx('SubjectContents-Content') }>
-          <div className={ cx('SubjectContents-Content', { SubjectT: MainColor, SubjectS: !MainColor }) }>과목</div>
+class SubjectContents extends Component {
+  render () {
+    const { SubjectData, MainMember } = this.props;
+    const SubjectList = SubjectData;
+    const MainColor = MainMember;
+    return (
+      <Fragment>
+        <div className={ cx('SubjectContents') }>
+          <div className={ cx('SubjectContents-Content') }>
+            <div className={ cx('SubjectContents-Content', { SubjectT: MainColor, SubjectS: !MainColor }) }>과목</div>
+          </div>
+          <div className={ cx('SubjectContents-SubjectCardList') }>
+            {
+              SubjectList.map((Subject, i) => {
+                return (
+                  <SubjectCard SubjectTitle={ Subject } key={ i } />
+                );
+              })
+            }
+          </div>
         </div>
-        <div className={ cx('SubjectContents-SubjectCardList') }>
-          {
-            SubjectList.map((Subject, i) => {
-              return (
-                <SubjectCard SubjectTitle={ Subject } key={ i } />
-              );
-            })
-          }
-        </div>
-      </div>
-    </Fragment>
-  );
-};
+      </Fragment>
+    );
+  }
+}
 
 export default SubjectContents;
--- Hun-Min-Jeong-Eum-Front/src/containers/Main/Teacher/TeacherContainer.js
+++ Hun-Min-Jeong-Eum-Front/src/containers/Main/Teacher/TeacherContainer.js
@@ -31,7 +31,7 @@ class TeacherContainer extends Component {
         { Subject: '수학 필기', writeTime: '2019.07.27', classInfo: '2학년 3반' },
         { Subject: '수학 필기', writeTime: '2019.07.26', classInfo: '2학년 4반' }
       ],
-      MainColor: true
+      MainMember: true
     };
   }
   OnClickLogOut = () => {
@@ -50,7 +50,7 @@ class TeacherContainer extends Component {
             <span className={ cx('MainTeacher-Header-Made') }>Made By 나랏말싸미</span>
           </div>
           {
-            this.props.store.MCCS.isContents === true ? <SubjectContents SubjectData={ this.state.SubjectData } MainColor={ this.state.MainColor } /> : <TakeNoteContents TakeNoteData={ this.state.TakeNoteData } MainColor={ this.state.MainColor } />
+            this.props.store.MCCS.isContents === true ? <SubjectContents SubjectData={ this.state.SubjectData } MainMember={ this.state.MainMember } /> : <TakeNoteContents TakeNoteData={ this.state.TakeNoteData } MainMember={ this.state.MainMember } />
           }
         </div>
       </Fragment>
