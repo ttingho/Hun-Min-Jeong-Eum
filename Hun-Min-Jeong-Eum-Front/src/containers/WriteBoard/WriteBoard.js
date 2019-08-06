70bd924c3a21200468f28c17741897b4d6e879ce
ttingho <myeongho020812@gmail.com>
2019-08-06 23:42:29 +0900

    [ADD] WriteBoard
    WriteBoard 페이지 관련 파일 생성 및 코딩

--- Hun-Min-Jeong-Eum-Front/src/containers/WriteBoard/WriteBoard.js
+++ Hun-Min-Jeong-Eum-Front/src/containers/WriteBoard/WriteBoard.js
@@ -1,43 +1,80 @@
-import React, { Component } from 'react';
-import './WriteBoard.scss';
-import cancle from '../../images/Icon/cancle.png';
-import bring from '../../images/Icon/bring.png';
-import pen from '../../images/Icon/pen.png';
-import highlight from '../../images/Icon/highlight.png';
-import eraser from '../../images/Icon/eraser.png';
-import save from '../../images/Icon/save.png';
-import Canvas from '../Canvas';
+import React, { Component, Fragment } from 'react';
+import style from './WriteBoard.scss';
+import classNames from 'classnames/bind';
+import { IconContext } from 'react-icons';
+import { MdClose, MdPhotoSizeSelectActual, MdBrush, MdFileDownload, MdUndo } from 'react-icons/md';
+import CanvasDraw from 'react-canvas-draw';
+import { withRouter } from 'react-router-dom';
+
+const cx = classNames.bind(style);
 
 class WriteBoard extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      colorPick: '#FFFFFF',
+      BackImg: 'http://image.news1.kr/system/photos/2018/8/29/3274576/article.jpg'
+    };
+  }
+  onChangeColor = (e) => {
+    this.setState({
+      colorPick: e.target.value
+    });
+  }
+  onChangeUndo = () => {
+    this.saveableCanvas.undo();
+  }
+  onClickMainPage = () => {
+    this.props.history.push('/');
+  }
   render () {
     return (
-      <div className="WriteBoard">
-        <div className="WriteBoard-header">
-          <img className="WriteBoard-header-cancle" src={cancle} alt=""/>
-          <img className="WriteBoard-header-bring" src={bring} alt=""/>
-          <div className="WriteBoard-header-tools">
-            <img className="WriteBoard-header-tools-pen" src={pen} alt=""/>
-            <img className="WriteBoard-header-tools-highlight" src={highlight} alt=""/>
-            <img className="WriteBoard-header-tools-eraser" src={eraser} alt=""/>
-            <div className="WriteBoard-header-tools-Color">
-              <div className="WriteBoard-header-tools-Color-current">
-                <div className="WriteBoard-header-tools-Color-current-detail"></div>
+      <Fragment>
+        <div className={ cx('WriteBoard') }>
+          <div className={ cx('WriteBoard-Header') }>
+            <IconContext.Provider value={{ color: 'white', size: '3rem', className: 'MdButton' }}>
+              <MdClose onClick={ this.onClickMainPage } />
+            </IconContext.Provider>
+            <IconContext.Provider value={{ color: 'white', size: '3rem', className: 'MdButton' }}>
+              <MdPhotoSizeSelectActual />
+            </IconContext.Provider>
+            <IconContext.Provider value={{ color: 'white', size: '3rem', className: 'MdButton' }}>
+              <MdUndo onClick={() => {
+                this.saveableCanvas.undo();
+              }} />
+            </IconContext.Provider>
+            <div className={ cx('draw') }>
+              <IconContext.Provider value={{ color: this.state.colorPick, size: '3rem', className: 'MdButton' }}>
+                <MdBrush />
+              </IconContext.Provider>
+              <div className={ cx('ColorPicker') }>
+                <input className={ cx('color1') } type='button' value='#000000' onClick={ this.onChangeColor } />
+                <input className={ cx('color2') } type='button' value='#FF0000' onClick={ this.onChangeColor } />
+                <input className={ cx('color3') } type='button' value='#5A5AFF' onClick={ this.onChangeColor } />
+                <input className={ cx('color4') } type='button' value='#FFC81E' onClick={ this.onChangeColor } />
+                <input className={ cx('color5') } type='button' value='#FFFFFF' onClick={ this.onChangeColor } />
               </div>
-              <div className="WriteBoard-header-tools-Color-black"></div>
-              <div className="WriteBoard-header-tools-Color-red"></div>
-              <div className="WriteBoard-header-tools-Color-blue"></div>
             </div>
+            <IconContext.Provider value={{ color: 'white', size: '3rem', className: 'MdButton' }}>
+              <MdFileDownload />
+            </IconContext.Provider>
           </div>
-          <img className="WriteBoard-header-save" src={save} alt=""/>
-        </div>
-        <div className="WriteBoard-contents">
-          <div className="WriteBoard-contents-box">
-            <Canvas />
+          <div className={ cx('WriteBoard-Contents') }>
+            <CanvasDraw
+              brushRadius={ 5 }
+              brushColor={ this.state.colorPick }
+              catenaryColor={ this.state.colorPick }
+              canvasWidth={ 1900 }
+              canvasHeight={ 850 }
+              hideGrid={ true }
+              imgSrc={ this.state.BackImg }
+              ref={canvasDraw => (this.loadableCanvas = canvasDraw)}
+            />
           </div>
         </div>
-      </div>
+      </Fragment>
     );
   }
 }
 
-export default WriteBoard;
+export default withRouter(WriteBoard);
